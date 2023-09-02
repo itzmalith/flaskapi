5e0ac2824945c16f0b2797b380bcf688ee4f78ce
itzmalith <84328437+itzmalith@users.noreply.github.com>
2023-09-02 20:02:06 +0530

    flaskbackend
    flask backend added

---
+++ flsk/app.py
@@ -0,0 +1,41 @@
+# Import necessary libraries
+from flask import Flask, request, jsonify
+import numpy as np
+import joblib
+from flask_cors import CORS
+
+# Create a Flask app
+app = Flask(__name__)
+CORS(app, resources={r"/predict": {"origins": "*"}})  
+
+# Load the trained model
+model = joblib.load('/Users/malithlekamge/Documents/ATLINKANGULAR/AUTH/flsk/heart_disease_model.pkl')
+
+# Define a route for prediction
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Get input data from the request
+        input_data = request.json['data']
+        
+        # Perform the same preprocessing as before
+        input_data_as_numpy_array = np.asarray(input_data)
+        input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+        
+        # Make a prediction
+        prediction = model.predict(input_data_reshaped)
+        
+        # Return the prediction result
+        if prediction[0] == 0:
+            result = '0'
+        else:
+            result = '1'
+            
+        return jsonify({'result': result})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
+
